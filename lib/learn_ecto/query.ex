12e025278bc94f3fd0dbdcdcd21315a87708ecf8
Daniel André <dap1995@gmail.com>
2016-09-09 20:34:38 -0300

    Added some user queries and for each query I have created a simple test to empty database

--- lib/learn_ecto/query.ex
+++ lib/learn_ecto/query.ex
@@ -1,9 +1,50 @@
 defmodule LearnEcto.Query do
 import Ecto.Query, only: [from: 2]
-LearnEcto.Repo.start_link()
-query = from u in "users",
-  where: u.email == "dap1995@gmail.com",
-  select: u.username
+alias LearnEcto.{Repo, User}
 
-LearnEcto.Repo.all(query)
+#List all confirmed users
+def listConfirmedUsers do
+Repo.start_link()
+query = from u in User,
+  where: u.confirmed == true,
+  select: u.username
+Repo.all(query)
+end
+
+#Count all confirmed users
+def countConfirmedUsers do
+  Repo.start_link()
+  query = from u in User,
+  where: u.confirmed == true,
+  select: count(u.id)
+  Repo.all(query)
+end
+
+#Count the confirmed and not confirmed group
+def countConfirmedUsersGroup do
+  Repo.start_link()
+  query = from u in User,
+  group_by: u.confirmed,
+  select: [u.confirmed, count(u.id)]
+  Repo.all(query)
+end
+
+#List all users by date
+def listUsersByDate do
+  Repo.start_link()
+  query = from u in User,
+  order_by: u.inserted_at,
+  select: [u.username, u.inserted_at]
+  Repo.all(query)
+end
+
+#List all user by decreasing date
+def listUsersByDecreasingDate do
+  Repo.start_link()
+  query = from u in User,
+  order_by: [desc: u.inserted_at],
+  select: [u.username, u.inserted_at]
+  Repo.all(query)
+end
+
 end
--- test/learn_ecto_test.exs
+++ test/learn_ecto_test.exs
@@ -1,8 +1,27 @@
 defmodule LearnEctoTest do
   use ExUnit.Case
+  alias LearnEcto.Query
   doctest LearnEcto
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  #This is a simple test with a empty database
+  test "list Confirmed Users" do
+    assert Query.listConfirmedUsers == []
+  end
+
+  #This is a simple test with a empty database
+  test "count Confirmed Users" do
+    assert Query.countConfirmedUsers == [0]
+  end
+
+  test "count Confirmed Users Group" do
+    assert Query.countConfirmedUsersGroup == []
+  end
+
+  test "list Users By Date" do
+    assert Query.listUsersByDate == []
+  end
+
+  test "list Users By Decreasing Date" do
+    assert Query.listUsersByDecreasingDate == []
   end
 end
